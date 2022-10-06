accfc10bd906b9956a3b8a4e17dc2d2123cddb83
aehtiopicus <aehtiopicus@gmail.com>
2022-10-06 16:10:42 -0300

    Imported from Quest

--- src/assets/assets/fonts/src/App.tsx
+++ src/assets/assets/fonts/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/QuestDesignSystem5LightTheme';
-import OtherCom from 'src/components/OtherCom/OtherCom';
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={questTheme}>
         <Routes>
-          <Route path="/" element={<OtherCom/>} />
         </Routes>
       </ThemeProvider>
     </StyledEngineProvider>
